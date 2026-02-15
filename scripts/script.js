254d1ed48dbdde872b95b476ec9cc5f98a6beea5
drasti492 <modes7267@gmail.com>
2026-02-15 16:27:04 +0300

    improve phone input UI +263 format

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,29 @@
 const form = document.getElementById("loanForm");
 const successMessage = document.getElementById("successMessage");
+const phoneInput = document.getElementById("phoneInput");
 
+
+// FORCE NUMBER START WITH 7
+if (phoneInput) {
+  phoneInput.addEventListener("input", () => {
+
+    // remove non-numbers
+    phoneInput.value = phoneInput.value.replace(/\D/g, "");
+
+    // must start with 7
+    if (!phoneInput.value.startsWith("7")) {
+      phoneInput.value = "7";
+    }
+
+    // max length 9 digits
+    if (phoneInput.value.length > 9) {
+      phoneInput.value = phoneInput.value.slice(0, 9);
+    }
+  });
+}
+
+
+// SUBMIT UI
 if (form) {
   form.addEventListener("submit", () => {
 
@@ -12,8 +35,5 @@ if (form) {
     const button = form.querySelector("button");
     button.disabled = true;
     button.innerText = "Processing...";
-
-    // DO NOT redirect here.
-    // Backend will redirect after saving.
   });
 }
